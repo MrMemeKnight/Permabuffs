4bed5337c2ba7289bb3f6f83297b9b8a9bc7daa7
MemeKnight <57252398+MrMemeKnight@users.noreply.github.com>
2025-07-22 20:03:01 +0800

    Update PermaBuffs.cs

--- Permabuffs/PermaBuffs.cs
+++ Permabuffs/PermaBuffs.cs
@@ -1,92 +1,78 @@
 using System;
+using System.Collections.Generic;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
-using System.Collections.Generic;
-using System.Linq;
+using OTAPI;
 
 namespace Permabuffs
 {
     [ApiVersion(2, 1)]
-    public class PermaBuffs : TerrariaPlugin
+    public class Permabuffs : TerrariaPlugin
     {
-        public override string Name => "PermaBuffs";
+        public override string Author => "Original: SyntaxVoid | Restored by ChatGPT";
+        public override string Name => "Permabuffs";
         public override Version Version => new Version(1, 0);
-        public override string Author => "SyntaxVoid (Modified)";
-        public override string Description => "Allows permanent buffs from potions stored in Piggy Bank";
+        public override string Description => "Grants potion buffs from piggy bank if 30+ potions are present.";
 
-        public PermaBuffs(Main game) : base(game) { }
+        public Permabuffs(Main game) : base(game) { }
+
+        public static Dictionary<int, bool> EnabledPlayers = new();
 
         public override void Initialize()
         {
-            // STEP 2: Skip DB.Connect() to test if it causes crash
-            // DB.Connect();
-
-            Commands.ChatCommands.Add(new Command("permabuffs.manage", PermaBuffCommand, "permabuff"));
-
-            // STEP 1: Already tested disabling OnUpdate
-            // ServerApi.Hooks.GameUpdate.Register(this, OnUpdate);
+            ServerApi.Hooks.GameUpdate.Register(this, OnUpdate);
+            Commands.ChatCommands.Add(new Command("permabuffs.toggle", ToggleBuffs, "pbenable", "pbdisable"));
         }
 
-        private void PermaBuffCommand(CommandArgs args)
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.GameUpdate.Deregister(this, OnUpdate);
+            }
+            base.Dispose(disposing);
+        }
+
+        private void ToggleBuffs(CommandArgs args)
         {
             var player = args.Player;
 
-            if (args.Parameters.Count < 1)
+            if (args.Message.StartsWith("/pbenable"))
             {
-                player.SendInfoMessage("Usage: /permabuff <add/remove> <buffname>");
-                return;
+                EnabledPlayers[player.Index] = true;
+                player.SendSuccessMessage("Permabuffs enabled.");
             }
-
-            int buffId;
-            switch (args.Parameters[0].ToLower())
+            else if (args.Message.StartsWith("/pbdisable"))
             {
-                case "add":
-                    if (args.Parameters.Count < 2)
-                    {
-                        player.SendErrorMessage("Specify a buff name to add.");
-                        return;
-                    }
-                    if (!Potions.buffIDs.TryGetValue(args.Parameters[1].ToLower(), out buffId))
-                    {
-                        player.SendErrorMessage("Invalid buff name.");
-                        return;
-                    }
-                    // DB.AddBuff(player.Account.ID, buffId);
-                    player.SendSuccessMessage($"[TEST MODE] Would add permanent buff: {args.Parameters[1]}");
-                    break;
-
-                case "remove":
-                    if (args.Parameters.Count < 2)
-                    {
-                        player.SendErrorMessage("Specify a buff name to remove.");
-                        return;
-                    }
-                    if (!Potions.buffIDs.TryGetValue(args.Parameters[1].ToLower(), out buffId))
-                    {
-                        player.SendErrorMessage("Invalid buff name.");
-                        return;
-                    }
-                    // DB.RemoveBuff(player.Account.ID, buffId);
-                    player.SendSuccessMessage($"[TEST MODE] Would remove permanent buff: {args.Parameters[1]}");
-                    break;
-
-                default:
-                    player.SendErrorMessage("Unknown subcommand.");
-                    break;
+                EnabledPlayers[player.Index] = false;
+                player.SendSuccessMessage("Permabuffs disabled.");
             }
         }
 
         private void OnUpdate(EventArgs args)
         {
-            foreach (var player in TShock.Players.Where(p => p != null && p.Active && p.RealPlayer))
+            foreach (var tsPlayer in TShock.Players)
             {
-                var buffs = DB.GetBuffs(player.Account.ID);
-                foreach (var buffId in buffs)
+                if (tsPlayer == null || !tsPlayer.Active || tsPlayer.TPlayer == null || !EnabledPlayers.ContainsKey(tsPlayer.Index) || !EnabledPlayers[tsPlayer.Index])
+                    continue;
+
+                var player = tsPlayer.TPlayer;
+                var piggyBank = player.bank.item;
+
+                foreach (var potion in PotionToBuff.Map)
                 {
-                    if (!player.TPlayer.buffType.Contains(buffId))
+                    int stackCount = 0;
+
+                    foreach (var item in piggyBank)
                     {
-                        player.SetBuff(buffId, 60 * 2); // 2 seconds
+                        if (item != null && item.active && item.type == potion.Key)
+                            stackCount += item.stack;
+                    }
+
+                    if (stackCount >= 30 && !player.HasBuff(potion.Value))
+                    {
+                        player.AddBuff(potion.Value, 60);
                     }
                 }
             }
