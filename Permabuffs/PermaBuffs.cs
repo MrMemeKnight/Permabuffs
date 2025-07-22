a14831c4efd932af1b36442f5fcc56d8b0e16a2f
MemeKnight <57252398+MrMemeKnight@users.noreply.github.com>
2025-07-22 18:53:27 +0800

    Update PermaBuffs.cs

--- Permabuffs/PermaBuffs.cs
+++ Permabuffs/PermaBuffs.cs
@@ -3,32 +3,28 @@ using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using OTAPI;
 using Terraria;
+using Terraria.ID;
 using TerrariaApi.Server;
 using TShockAPI;
+using Permabuffs;
 
 namespace Permabuffs
 {
     [ApiVersion(2, 1)]
-    public class Permabuffs : TerrariaPlugin
+    public class PermaBuffs : TerrariaPlugin
     {
-        public override string Name => "Permabuffs";
-        public override Version Version => new Version(1, 0);
-        public override string Author => "Myoni (SyntaxVoid)";
-        public override string Description => "Piggy bank permabuffs!";
+        public override string Name => "PermaBuffs";
+        public override Version Version => new Version(1, 1, 0);
+        public override string Author => "Myoni";
+        public override string Description => "Permanent potion effects for TShock";
 
-        public Permabuffs(Main game) : base(game)
-        {
-        }
+        public PermaBuffs(Main game) : base(game) { }
 
         public override void Initialize()
         {
             ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
             ServerApi.Hooks.ServerJoin.Register(this, OnJoin);
-
-            Commands.ChatCommands.Add(new Command("permbuff", CmdPermabuff, "permabuff"));
-
-            Potions.Initialize();
-            DB.Load();
+            ServerApi.Hooks.GamePostUpdate.Register(this, OnUpdate);
         }
 
         protected override void Dispose(bool disposing)
@@ -37,68 +33,45 @@ namespace Permabuffs
             {
                 ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
                 ServerApi.Hooks.ServerJoin.Deregister(this, OnJoin);
+                ServerApi.Hooks.GamePostUpdate.Deregister(this, OnUpdate);
             }
             base.Dispose(disposing);
         }
 
         private void OnInitialize(EventArgs args)
         {
-            DB.Load();
+            DB.Connect();
+            NameToBuffIDs.PopulateBuffIDs();
         }
 
         private void OnJoin(JoinEventArgs args)
         {
-            var player = TShock.Players[args.Who];
-            if (player == null || !player.Active)
-                return;
-
-            List<int> buffs = DB.Get(args.Who);
-            foreach (int buff in buffs)
+            TSPlayer player = TShock.Players[args.Who];
+            if (player != null && player.Active)
             {
-                player.SetBuff(buff, 60 * 60 * 30); // 30-minute duration (auto-refreshed)
+                DB.CreatePlayer(player);
             }
         }
 
-        private void CmdPermabuff(CommandArgs args)
+        private void OnUpdate(EventArgs args)
         {
-            if (args.Parameters.Count != 1)
+            foreach (TSPlayer player in TShock.Players)
             {
-                args.Player.SendErrorMessage("Usage: /permabuff <potion name>");
-                return;
-            }
+                if (player == null || !player.Active || player.TPlayer.dead)
+                    continue;
 
-            string potionName = args.Parameters[0].ToLowerInvariant();
+                List<int> permBuffs = DB.GetBuffs(player.Name);
+                if (permBuffs == null)
+                    continue;
 
-            if (!Potions.NameToBuffIDs.ContainsKey(potionName))
-            {
-                args.Player.SendErrorMessage("Invalid potion name.");
-                return;
-            }
-
-            int buffID = Potions.NameToBuffIDs[potionName];
-            int itemType = Potions.PotionToItem[potionName];
-
-            var piggy = args.Player.TPlayer.bank.item;
-
-            int totalCount = 0;
-            foreach (var item in piggy)
-            {
-                if (item != null && item.type == itemType)
+                foreach (int buffID in permBuffs)
                 {
-                    totalCount += item.stack;
+                    if (buffID < Main.buffName.Length && !player.TPlayer.HasBuff(buffID))
+                    {
+                        player.TPlayer.AddBuff(buffID, 18010, true);
+                    }
                 }
             }
-
-            if (totalCount < 30)
-            {
-                args.Player.SendErrorMessage("You need at least 30 of that potion in your piggy bank.");
-                return;
-            }
-
-            DB.Add(args.Player.Index, buffID);
-            args.Player.SendSuccessMessage("Permanent buff added: " + potionName);
-
-            args.Player.SetBuff(buffID, 60 * 60 * 30);
         }
     }
 }
