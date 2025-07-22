020a3be49a3164d9f7da4c5bdf10055f825e2012
MemeKnight <57252398+MrMemeKnight@users.noreply.github.com>
2025-07-22 22:52:05 +0800

    Update DB.cs

--- Permabuffs/DB.cs
+++ Permabuffs/DB.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Data;
 using TShockAPI.DB;
 
@@ -6,48 +7,47 @@ namespace Permabuffs
 {
     public class DB
     {
-        private readonly IDbConnection _db;
+        private IDbConnection _db;
 
         public DB(IDbConnection db)
         {
             _db = db;
 
             var table = new SqlTable("Permabuffs",
-                new SqlColumn("UserID", MySqlDbType.Int32) { Primary = true },
-                new SqlColumn("Enabled", MySqlDbType.Int32)
+                new SqlColumn("UserID", DbType.Int32) { Primary = true },
+                new SqlColumn("Enabled", DbType.Int32)
             );
 
             var creator = new SqlTableCreator(_db,
-                db.GetSqlType() == SqlType.Sqlite
-                    ? new SqliteQueryCreator()
-                    : new MysqlQueryCreator());
+                _db.GetSqlType() == SqlType.Sqlite
+                    ? new SqliteQueryCreator(_db)
+                    : new MysqlQueryCreator(_db));
 
             creator.EnsureTableStructure(table);
         }
 
-        public void SetState(int userId, bool enabled)
-        {
-            var exists = _db.QueryReader("SELECT * FROM Permabuffs WHERE UserID = @0", userId).Read();
-
-            if (exists)
-            {
-                _db.Query("UPDATE Permabuffs SET Enabled = @0 WHERE UserID = @1", enabled ? 1 : 0, userId);
-            }
-            else
-            {
-                _db.Query("INSERT INTO Permabuffs (UserID, Enabled) VALUES (@0, @1)", userId, enabled ? 1 : 0);
-            }
-        }
-
-        public bool GetState(int userId)
+        public bool IsEnabled(int userId)
         {
             using var reader = _db.QueryReader("SELECT Enabled FROM Permabuffs WHERE UserID = @0", userId);
             if (reader.Read())
-            {
                 return reader.Get<int>("Enabled") == 1;
-            }
 
             return false;
+        }
+
+        public void SetEnabled(int userId, bool enabled)
+        {
+            int val = enabled ? 1 : 0;
+            if (HasUser(userId))
+                _db.Query("UPDATE Permabuffs SET Enabled = @0 WHERE UserID = @1", val, userId);
+            else
+                _db.Query("INSERT INTO Permabuffs (UserID, Enabled) VALUES (@0, @1)", userId, val);
+        }
+
+        private bool HasUser(int userId)
+        {
+            using var reader = _db.QueryReader("SELECT 1 FROM Permabuffs WHERE UserID = @0", userId);
+            return reader.Read();
         }
     }
 }
