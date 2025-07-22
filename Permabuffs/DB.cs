b5cd681a50ef9cc1bb32c95b5824ffa3848f6188
MemeKnight <57252398+MrMemeKnight@users.noreply.github.com>
2025-07-22 21:42:30 +0800

    Update DB.cs

--- Permabuffs/DB.cs
+++ Permabuffs/DB.cs
@@ -1,47 +1,53 @@
+using System.Collections.Generic;
+using Mono.Data.Sqlite;
 using System.Data;
-using TShockAPI.DB;
+using TShockAPI;
 
 namespace Permabuffs
 {
-    public class DB
+    public static class DB
     {
-        private IDbConnection db;
+        public static Dictionary<int, bool> ToggleStatus = new Dictionary<int, bool>();
 
-        public DB()
+        public static bool GetStatus(int userID)
         {
-            db = TShock.DB;
-            var table = new SqlTable("Permabuffs",
-                new SqlColumn("UserID", MySql.Data.MySqlClient.MySqlDbType.Int32) { Primary = true },
-                new SqlColumn("Enabled", MySql.Data.MySqlClient.MySqlDbType.Int32));
-            var creator = new SqlTableCreator(db,
-                db.GetSqlType() == SqlType.Sqlite
-                    ? new SqliteQueryCreator(db, TShock.Config.Settings.TablePrefix)
-                    : new MysqlQueryCreator(db, TShock.Config.Settings.TablePrefix));
-            creator.EnsureTableStructure(table);
+            using (var db = new SqliteConnection($"uri=file={TShock.SavePath}/permabuffs.sqlite"))
+            {
+                db.Open();
+                var cmd = db.CreateCommand();
+                cmd.CommandText = "CREATE TABLE IF NOT EXISTS PermabuffStatus (UserID INTEGER PRIMARY KEY, Enabled BOOLEAN);";
+                cmd.ExecuteNonQuery();
+
+                cmd.CommandText = "SELECT Enabled FROM PermabuffStatus WHERE UserID = @0;";
+                cmd.Parameters.AddWithValue("@0", userID);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return reader.GetBoolean(0);
+                    }
+                }
+
+                // Default disabled
+                return false;
+            }
         }
 
-        public bool PlayerExists(int userId)
+        public static void SetStatus(int userID, bool enabled)
         {
-            using var reader = db.QueryReader("SELECT * FROM Permabuffs WHERE UserID = @0", userId);
-            return reader.Read();
-        }
+            using (var db = new SqliteConnection($"uri=file={TShock.SavePath}/permabuffs.sqlite"))
+            {
+                db.Open();
+                var cmd = db.CreateCommand();
+                cmd.CommandText = "CREATE TABLE IF NOT EXISTS PermabuffStatus (UserID INTEGER PRIMARY KEY, Enabled BOOLEAN);";
+                cmd.ExecuteNonQuery();
 
-        public void AddPlayer(int userId)
-        {
-            db.Query("INSERT INTO Permabuffs (UserID, Enabled) VALUES (@0, @1)", userId, 0);
-        }
-
-        public bool IsEnabled(int userId)
-        {
-            using var reader = db.QueryReader("SELECT Enabled FROM Permabuffs WHERE UserID = @0", userId);
-            if (reader.Read())
-                return reader.Get<int>("Enabled") == 1;
-            return false;
-        }
-
-        public void SetEnabled(int userId, bool enabled)
-        {
-            db.Query("UPDATE Permabuffs SET Enabled = @0 WHERE UserID = @1", enabled ? 1 : 0, userId);
+                cmd.CommandText = "INSERT OR REPLACE INTO PermabuffStatus (UserID, Enabled) VALUES (@0, @1);";
+                cmd.Parameters.AddWithValue("@0", userID);
+                cmd.Parameters.AddWithValue("@1", enabled);
+                cmd.ExecuteNonQuery();
+            }
         }
     }
 }
